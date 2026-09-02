37ce078c1b5d202572deb48d7b3cb46799b24352
Valery777 <rjotkevich@yahoo.com>
2026-09-02 20:48:10 +0300

    Start .NET Product API in ClientRun.bat
    Added command to start the .NET Product API.

--- ClientRun.bat
+++ ClientRun.bat
@@ -1,4 +1,5 @@
 @echo off
+
 echo Starting React client...
 cd client-ts
 start cmd /k "npm start"
@@ -7,6 +8,9 @@ echo Starting Node.js orders API...
 cd ../orders-api
 start cmd /k "npm start"
 
+echo Starting .NET Product API...
+cd ../DotNetProductApi
+start cmd /k "dotnet run"
+
 echo All services started.
 pause
-
